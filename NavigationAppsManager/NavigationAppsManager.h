919f8a45ccb680760a7e6b5d51af02d2f72c57f2
Angel Antonov <>
2019-05-19 11:40:32 +0300

    Initial commit

---
+++ NavigationAppsManager/NavigationAppsManager.h
@@ -0,0 +1,19 @@
+//
+//  NavigationAppsManager.h
+//  NavigationAppsManager
+//
+//  Created by Angel Antonov on 19.05.19.
+//  Copyright © 2019 Angel Antonov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NavigationAppsManager.
+FOUNDATION_EXPORT double NavigationAppsManagerVersionNumber;
+
+//! Project version string for NavigationAppsManager.
+FOUNDATION_EXPORT const unsigned char NavigationAppsManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NavigationAppsManager/PublicHeader.h>
+
+
